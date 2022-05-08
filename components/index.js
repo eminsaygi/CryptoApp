40e5a9ca295b6e25943c1a75ff5dff5a1e834f7f
Emin Saygı <emin_0001@hotmail.com>
2022-05-08 15:03:58 +0300

    Text Button added

--- components/index.js
+++ components/index.js
@@ -1,5 +1,6 @@
 import PriceAlert from './PriceAlert';
 import TransactionsHistory from './TransactionsHistory';
 import HeaderBar from './HeaderBar';
-
-export {PriceAlert, TransactionsHistory, HeaderBar};
+import CurrencyLabel from './CurrencyLabel';
+import TextButton from './TextButton';
+export {PriceAlert, TransactionsHistory, HeaderBar, CurrencyLabel, TextButton};
