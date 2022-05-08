39673204b576d7db8c957854707067ade4ce1908
Emin Saygı <emin_0001@hotmail.com>
2022-05-08 12:38:05 +0300

    Header Bar components added in index

--- components/index.js
+++ components/index.js
@@ -1,4 +1,5 @@
 import PriceAlert from './PriceAlert';
 import TransactionsHistory from './TransactionsHistory';
+import HeaderBar from './HeaderBar';
 
-export {PriceAlert, TransactionsHistory};
+export {PriceAlert, TransactionsHistory, HeaderBar};
